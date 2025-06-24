971dd9267d443f3f7504d57c9fc3a1bc41746a22
Nanada-nk <nada.nidchakul@gmail.com>
2025-06-24 13:42:09 +0700

    not-found & error middleware

---
+++ src/middlewares/error.middleware.js
@@ -0,0 +1,11 @@
+export default (err, req, res, next) => {
+  console.log(err.name)
+  err.customMsg = "CC20"
+  err.statusCode = 500
+  res.status(err.statusCode)
+    .json({
+      errorName: err.name,
+      error: err.message,
+      customMsg: err.customMsg
+    })
+}
